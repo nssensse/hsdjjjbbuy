b0c32c80801c47f6aea51b1a5a721d550ef02325
Vladimir <nightsense@mail.ru>
2024-01-27 03:59:48 +0300

    dddddddsfs

--- src/app/show/page.js
+++ src/app/show/page.js
@@ -57,7 +57,41 @@ export default async function Home() {
         >
           Submit
         </button>
-      </form>
+      </form><h1 className="text-xl font-bold">Notes</h1>
+      <div>
+        <ul className="flex font-bold">
+          <li className="flex-1">Title</li>
+          <li className="flex-1">Note</li>
+          <li className="flex-1">Options</li>
+        </ul>
+        <hr />
+        {notes.map((element) => {
+          return (
+            <>
+              <ul key={element._id} className="flex">
+                <li className="flex-1">{element.title}</li>
+                <li className="flex-1">{element.note}</li>
+                <li className="flex-1">
+                  <div className="flex">
+                    <form action={deleteNote}>
+                        <input type="hidden" value={JSON.stringify(element._id)} name="id"/>
+                      <button
+                        type="submit"
+                        className="p-2 m-2 bg-red-600 text-white hover:cursor-pointer"
+                      >
+                        Delete
+                      </button>
+                    </form>
+                    {/* <Delete id={element._id}/> */}
+ 
+                  </div>
+                </li>
+              </ul>
+              <hr />
+            </>
+          );
+        })}
+      </div>
     </main>
   );
 }
