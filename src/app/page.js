6d8ddeb0131f9b7d91ab62efc0280dba285f3a4a
Vladimir <nightsense@mail.ru>
2024-01-27 04:39:23 +0300

    ddddddfssdf

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Delete from "./components/Delete";
 import Link from "next/link";
 //////widget
 export default async function show() {
-  dbConnect()
+  dbConnect();
   const notes = await Note.find();
 
   async function deleteNote(data) {
@@ -17,46 +17,96 @@ export default async function show() {
   }
 
   return (
-    <main className="m-10 space-y-5">
-      <h1 className="text-xl font-bold">Notes</h1>
-      <div>
-        <ul className="flex font-bold">
-          <li className="flex-1">Title</li>
-          <li className="flex-1">Note</li>
-          <li className="flex-1">Options</li>
-        </ul>
-        <hr />
-        {notes.map((element) => {
-          return (
-            <>
-              <ul key={element._id} className="flex">
-                <li className="flex-1">{element.title}</li>
-                <li className="flex-1">{element.note}</li>
-                <li className="flex-1">
-                  <div className="flex">
-                    <form action={deleteNote}>
-                        <input type="hidden" value={JSON.stringify(element._id)} name="id"/>
-                      <button
-                        type="submit"
-                        className="p-2 m-2 bg-red-600 text-white hover:cursor-pointer"
-                      >
-                        Delete
-                      </button>
-                    </form>
-                    {/* <Delete id={element._id}/> */}
-                    <Link href={"/Edit/" + element._id}>
-                      <button className="p-2 m-2 bg-blue-600 text-white hover:cursor-pointer">
-                        Edit
-                      </button>
-                    </Link>
+    <main className="main">
+
+        
+          <>
+            <div className="contcont">
+              <div className="main-header">
+                <div className="header-info">
+                  <div className="bbtittle">
+                    <div className="pic-logo"></div>
+                    <ul>BONUSBUY</ul>
                   </div>
-                </li>
-              </ul>
-              <hr />
-            </>
-          );
-        })}
-      </div>
+                  <div className="nowslot">⭐</div>
+                  <div className="topslot">🏆</div>
+                  <table id="table_fixed">
+                    <thead>
+                      <tr>
+                        <th>Balance</th>
+                        <th>💰</th>
+                      </tr>
+                      <tr>
+                        <th>Bonuses</th>
+
+                        <th> 🎁</th>
+                      </tr>
+                      <tr>
+                        <th>Profit</th>
+                        <th> 📈</th>
+                      </tr>
+                      <tr>
+                        <th>Status</th>
+                        <th>1.2X 📋</th>
+                      </tr>
+                    </thead>
+                  </table>
+                </div>
+              </div>
+
+              <table className="table tbdsfsd">
+                <thead className="thead-dark">
+                  <tr>
+                    <th width="10px"></th>
+                    <th width="350px">Slot name</th>
+                    <th width="30px">Bonus cost</th>
+                    <th width="40px">Bonus win</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                {notes.map((element) =>
+      {
+                  <ul key={element._id} className="flex">
+                    <li className="flex-1">{element.title}</li>
+                    <li className="flex-1">{element.note}</li>
+                    <li className="flex-1">
+                      <div className="flex">
+                        <form action={deleteNote}>
+                          <input
+                            type="hidden"
+                            value={JSON.stringify(element._id)}
+                            name="id"
+                          />
+                          <button
+                            type="submit"
+                            className="p-2 m-2 bg-red-600 text-white hover:cursor-pointer"
+                          >
+                            Delete
+                          </button>
+                        </form>
+                        {/* <Delete id={element._id}/> */}
+                        <Link href={"/Edit/" + element._id}>
+                          <button className="p-2 m-2 bg-blue-600 text-white hover:cursor-pointer">
+                            Edit
+                          </button>
+                        </Link>
+                      </div>
+                    </li>
+                  </ul>
+                  
+                })}               
+                </tbody>
+              </table>
+              <div className="container-bar">
+                <div className="progress2 progress-moved">
+                  <div className="progress-bar2"></div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      
     </main>
   );
 }
